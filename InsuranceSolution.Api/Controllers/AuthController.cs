05a2e2742e179b9435aab49d4805087ca488b4b8
Ahmad Mozaffar <aksoftware98@hotmail.com>
2022-08-13 16:48:24 +0400

    Create the register and login logic by issuing tokens

---
+++ InsuranceSolution.Api/Controllers/AuthController.cs
@@ -0,0 +1,84 @@
+﻿using InsuranceSolution.Shared.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace InsuranceSolution.Api.Controllers
+{
+    // api/auth 
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IConfiguration _config;
+        public AuthController(UserManager<IdentityUser> userManager, 
+                              IConfiguration config)
+        {
+            _userManager = userManager;
+            _config = config;
+        }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register([FromBody] RegisterUserRequest model)
+        {
+            // Validation 
+            var existingUser = await _userManager.FindByEmailAsync(model.Email);
+            if (existingUser != null)
+                return BadRequest("User already exists");
+
+            var user = new IdentityUser
+            {
+                Email = model.Email,
+                PhoneNumber = model.Phone,
+                UserName = model.Email,
+            };
+
+            await _userManager.CreateAsync(user, model.Password);
+
+            return Ok();
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginUserRequest model)
+        {
+            // Validation 
+            var user = await _userManager.FindByEmailAsync(model.Email);
+            if (user == null)
+                return BadRequest("Username or password is invalid");
+
+            var isPasswordValid = await _userManager.CheckPasswordAsync(user, model.Password);
+
+            // Generate the token 
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Email, model.Email),
+                new Claim(ClaimTypes.NameIdentifier, user.Id), // Name Identified = Id
+                new Claim(ClaimTypes.MobilePhone, user.PhoneNumber),
+            };
+
+            string keyAsString = _config["Jwt:Key"];
+            byte[] keyAsBytes = Encoding.UTF8.GetBytes(keyAsString); 
+            var key = new SymmetricSecurityKey(keyAsBytes);
+
+            var token = new JwtSecurityToken(
+                issuer: _config["Jwt:Issuer"],
+                audience: _config["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddDays(30),
+                signingCredentials: new SigningCredentials(key, SecurityAlgorithms.HmacSha256));
+
+            string tokenAsString = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return Ok(tokenAsString);
+        }
+    }
+}
---
+++ InsuranceSolution.Shared/Models/LoginUserRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace InsuranceSolution.Shared.Models
+{
+    public class LoginUserRequest
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
