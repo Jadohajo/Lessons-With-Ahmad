6dec0ec1edfe12e72c3a82f850610d36b1232816
Ahmad Mozaffar <aksoftware98@hotmail.com>
2022-08-13 16:48:02 +0400

    Setup the autnetication in startup.cs and update the database

--- InsuranceSolution.Api/Startup.cs
+++ InsuranceSolution.Api/Startup.cs
@@ -1,13 +1,16 @@
 using InsuranceSolution.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
@@ -18,7 +21,7 @@ namespace InsuranceSolution.Api
 {
     public class Startup
     {
-    
+
 
         public Startup(IConfiguration configuration)
         {
@@ -30,12 +33,37 @@ namespace InsuranceSolution.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options => {
-                options.UseSqlServer("Server=(localdb)\\MSSQLLocalDb;Database=InsuranceSolutionDb;Integrated Security=true", b => 
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer("Server=(localdb)\\MSSQLLocalDb;Database=InsuranceSolutionDb;Integrated Security=true", b =>
                 {
                     b.MigrationsAssembly("InsuranceSolution.Api");
-                }); 
+                });
+            }).AddIdentity<IdentityUser, IdentityRole>(options =>
+            {
+                // Here you can add your password configuration 
+                //options.Password.RequireNonAlphanumeric = true
+            }).AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders(); // Add the token providers comes with ASP.net Identity to genreated phone confirmation token, reset password token, confirm email token ...etc. 
+
+            services.AddAuthentication(auth =>
+            {
+                auth.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                auth.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = Configuration["Jwt:Issuer"],
+                    ValidAudience = Configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+                };
             });
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -47,7 +75,7 @@ namespace InsuranceSolution.Api
             {
                 options.AddPolicy("AllWebsites", policy =>
                 {
-                    policy.AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin(); 
+                    policy.AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin();
                 });
             });
 
